6842078168afe540f963af3a5f54f9532939565a
supriya gupta <supriyag75@gmail.com>
2024-07-11 12:53:45 +0530

    Put data in Widget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hive/hive.dart';
 
-class home_screen extends StatelessWidget {
+class home_screen extends StatefulWidget {
+  @override
+  State<home_screen> createState() => _home_screenState();
+}
+
+class _home_screenState extends State<home_screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +23,28 @@ class home_screen extends StatelessWidget {
               return Column(
                 children: [
                   ListTile(
-                    title: Text(snapshot.data!.get('name').toString()),
-                    subtitle: Text(snapshot.data!.get('age').toString()),
-                  ),
+                      title: Text(snapshot.data!.get('name').toString()),
+                      subtitle: Text(snapshot.data!.get('age').toString()),
+                      trailing: IconButton(
+                        onPressed: () {
+                          snapshot.data!.put('name', 'Shrreja Gupta');
+                          setState(() {});
+                        },
+                        icon: Icon(Icons.edit),
+                      )),
                   Text(snapshot.data!.get('user').toString()),
+                ],
+              );
+            },
+          ),
+          FutureBuilder(
+            future: Hive.openBox("arvind"),
+            builder: (context, snapshot) {
+              return Column(
+                children: [
+                  ListTile(
+                    title: Text(snapshot.data!.get('youtube').toString()),
+                  ),
                 ],
               );
             },
@@ -36,6 +60,10 @@ class home_screen extends StatelessWidget {
 
           print("Hive Database -- ${box.get('name')}");
           print("Hive Database -- ${box.get('age')}");
+
+          // Another Box
+          var box2 = await Hive.openBox('arvind');
+          box2.put('youtube', 'google.com');
         },
         child: Icon(Icons.add),
       ),
