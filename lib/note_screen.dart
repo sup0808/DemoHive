7561a09b6799afba7d91da24163cf6bab1e3feed
supriya gupta <supriyag75@gmail.com>
2024-07-12 08:39:53 +0530

    -- Delete notes from Box -- Edit notes from Box

--- lib/note_screen.dart
+++ lib/note_screen.dart
@@ -12,36 +12,59 @@ class note_screen extends StatefulWidget {
 }
 
 class _note_screenState extends State<note_screen> {
-  final titleController  = TextEditingController();
+  final titleController = TextEditingController();
   final descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Hive Database'),
       ),
-      body:ValueListenableBuilder(
+      body: ValueListenableBuilder(
         valueListenable: Boxes.getData().listenable(),
-        builder: (context,box ,_){
+        builder: (context, box, _) {
           var data = box.values.toList().cast<NotesModel>();
           return ListView.builder(
               itemCount: box.length,
-              itemBuilder: (context, index){
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(data[index].title.toString()),
-                    Text(data[index].description.toString()),
-
-                  ],
-                ),
-              ),
-            );
-          });
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(data[index].title.toString()),
+                            Spacer(),
+                            SizedBox(width: 15),
+                            InkWell(
+                                onTap: () {
+                                  deleteNote(data[index]);
+                                },
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                )),
+                            InkWell(
+                                onTap: () {
+                                  _editDialog(
+                                      data[index],
+                                      data[index].title.toString(),
+                                      data[index].description.toString());
+                                },
+                                child: Icon(Icons.edit)),
+                          ],
+                        ),
+                        Text(data[index].description.toString()),
+                      ],
+                    ),
+                  ),
+                );
+              });
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -65,17 +88,16 @@ class _note_screenState extends State<note_screen> {
                   TextFormField(
                     controller: titleController,
                     decoration: InputDecoration(
-                      hintText: "Enter Title" ,
-                      border: OutlineInputBorder()
-                    ),
+                        hintText: "Enter Title", border: OutlineInputBorder()),
                   ),
-                  SizedBox(height: 5,),
+                  SizedBox(
+                    height: 5,
+                  ),
                   TextFormField(
-                    controller: descriptionController ,
+                    controller: descriptionController,
                     decoration: InputDecoration(
-                        hintText: "Enter Description" ,
-                        border: OutlineInputBorder()
-                    ),
+                        hintText: "Enter Description",
+                        border: OutlineInputBorder()),
                   )
                 ],
               ),
@@ -88,16 +110,71 @@ class _note_screenState extends State<note_screen> {
                   child: Text("Cancel")),
               TextButton(
                   onPressed: () {
-                    final data = NotesModel(title: titleController.text, description: descriptionController.text);
+                    final data = NotesModel(
+                        title: titleController.text,
+                        description: descriptionController.text);
                     final box = Boxes.getData();
                     box.add(data);
-                  //  data.save();
+                    //  data.save();
                     print("Hive Database $box");
                     titleController.clear();
                     descriptionController.clear();
                     Navigator.pop(context);
                   },
                   child: Text("Add"))
+            ],
+          );
+        });
+  }
+
+  void deleteNote(NotesModel note) async {
+    await note.delete();
+  }
+
+  Future<void> _editDialog(
+      NotesModel notes, String title, String description) async {
+    titleController.text = title;
+    descriptionController.text = description;
+
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Edit Notes'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                        hintText: "Enter Title", border: OutlineInputBorder()),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(
+                        hintText: "Enter Description",
+                        border: OutlineInputBorder()),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Cancel")),
+              TextButton(
+                  onPressed: () async {
+                    notes.title = titleController.text.toString();
+                    notes.description = descriptionController.text.toString();
+                    await notes.save();
+                    Navigator.pop(context);
+                  },
+                  child: Text("Edit"))
             ],
           );
         });
